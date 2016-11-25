197533f9ebbbe5173c0f4ca0796c40f4bf8be674
tempires <vincent@tempires.com>
2016-11-25 14:55:54 +0800

    rocket_chat_mod v1.0

---
+++ packages/rocketchat-api/package.js
@@ -0,0 +1,24 @@
+Package.describe({
+	name: 'rocketchat:api',
+	version: '0.0.1',
+	summary: 'Rest API',
+	git: ''
+});
+
+Package.onUse(function(api) {
+	api.use([
+		'coffeescript',
+		'underscore',
+		'ecmascript',
+		'rocketchat:lib',
+		'nimble:restivus'
+	]);
+
+	api.addFiles('server/api.coffee', 'server');
+	api.addFiles('server/routes.coffee', 'server');
+	api.addFiles('server/settings.js', 'server');
+});
+
+Npm.depends({
+	busboy: '0.2.13'
+});
